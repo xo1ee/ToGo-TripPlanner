93225ce86a8629a1d477a2bedbcc6ed6abd1d2e5
ioSquishy <squishylxix@gmail.com>
2026-03-10 23:38:56 -0700

    removed trip map console logss

--- togo/components/trip-page/TripMap.tsx
+++ togo/components/trip-page/TripMap.tsx
@@ -15,27 +15,11 @@ interface MapViewProps {
 }
 
 export default function TripMap({ lat, lon, itinerary }: MapViewProps) {
-  console.log("Output coords:");
-  itinerary?.flatMap((days) =>
-    days.items.map((location) =>
-      console.log(
-        location.location?.locationLat + " " + location.location?.locationLon,
-      ),
-    ),
-  );
   return (
     <Map
       mapId={"d324cbd014b8ccf5e5e023e5"}
       defaultZoom={13}
       defaultCenter={{ lat: lat, lng: lon }}
-      onCameraChanged={(ev: MapCameraChangedEvent) =>
-        console.log(
-          "camera changed:",
-          ev.detail.center,
-          "zoom:",
-          ev.detail.zoom,
-        )
-      }
     >
       {itinerary?.flatMap((days) =>
         days.items.map((location) => (
