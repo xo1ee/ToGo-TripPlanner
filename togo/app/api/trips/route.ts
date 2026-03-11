b5b36f13e604b8e00e234c5ea4b4481cc8bcb9e0
ioSquishy <squishylxix@gmail.com>
2026-03-10 20:00:34 -0700

    Merge branch 'main' into add-destination-modal

---
+++ togo/app/api/trips/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/lib/firebase";
+import { Timestamp, collection, addDoc } from "firebase/firestore";
+const TRIPS_COLLECTION = "trips";
+
+export async function POST(req: NextRequest) {
+  try {
+    const formValues = await req.json();
+
+    // TODO: add value validation
+
+    // parse request body
+    const { location, startDate, endDate, users } = formValues;
+
+    const docRef = await addDoc(collection(db, TRIPS_COLLECTION), {
+      location,
+      startDate: startDate ? Timestamp.fromDate(new Date(startDate)) : null,
+      endDate: endDate ? Timestamp.fromDate(new Date(endDate)) : null,
+      users,
+      createdAt: new Date(),
+    });
+
+    return NextResponse.json(
+      { tripId: docRef.id, message: "Post created" },
+      { status: 201 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to create post" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function GET(req: NextRequest) {
+  //   return NextResponse.json({ trips }, { status: 200 });
+}
